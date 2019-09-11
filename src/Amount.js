ee9b04b44c6ace80d0bd76fb8a8aed383e572503
ppeou <ppeou@msn.com>
2019-09-10 23:55:53 -0500

    using material UI

--- src/Amount.js
+++ src/Amount.js
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
-import withStyles, {ThemeProvider} from 'react-jss';
+import React, {useState, useCallback} from 'react';
+import withStyles from 'react-jss';
+import Button from '@material-ui/core/Button';
 
-const styles =(theme) => ({
+const styles = (theme) => ({
   plusButton: {
     backgroundColor: theme.button.primary.backgroundColor,
     color: theme.button.primary.textColor,
   },
   minusButton: {
     backgroundColor: theme.button.danger.backgroundColor,
-      color: theme.button.danger.textColor
+    color: theme.button.danger.textColor
   }
 });
 
@@ -16,21 +17,22 @@ const Amount = ({classes, currencyList}) => {
   const [amount, setAmount] = useState(0);
 
   const updateAmount = (e) => {
-    const seed = Number(e.target.value);
+    const seed = Number(e.currentTarget.value);
     setAmount(amount + seed);
   };
+
 
   return (
     <div>
       <span>US Dollar: {amount} </span>
-      <button className={classes.plusButton} type="button"
-              value={1} onClick={updateAmount}>+</button>
+      <Button variant="contained" color="primary"
+              value={1} onClick={updateAmount}>+</Button>
       <span> | </span>
-      <button className={classes.minusButton} type="button"
-              value={-1} onClick={updateAmount}>-</button>
-      <hr />
-      {currencyList.map(CurrencyComponent => (
-        <CurrencyComponent amount={amount}/>
+      <Button variant="contained" color="secondary"
+              value={-1} onClick={updateAmount}>-</Button>
+      <hr/>
+      {currencyList.map((CurrencyComponent, idx) => (
+        <CurrencyComponent amount={amount} key={idx}/>
       ))}
     </div>
   );
